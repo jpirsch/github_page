0d0b9fc8699607e8f86b875996add4430585f26a
jpirsch <jeanpirsch@gmail.com>
2020-04-29 17:18:24 +0200

    Updates

---
+++ precache-manifest.fb0a1f06321fe8edf80a73be8a4c0c58.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1253b977aff4e1a2c43f59696829f2c2",
+    "url": "/github_page/index.html"
+  },
+  {
+    "revision": "78bcc1b45dda155b5279",
+    "url": "/github_page/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5e0324906c5efe7cc501",
+    "url": "/github_page/static/js/2.1a2d6d47.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github_page/static/js/2.1a2d6d47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78bcc1b45dda155b5279",
+    "url": "/github_page/static/js/main.43dd1aca.chunk.js"
+  },
+  {
+    "revision": "75bc375eee73792212c7",
+    "url": "/github_page/static/js/runtime-main.16e71cbd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github_page/static/media/logo.5d5d9eef.svg"
+  }
+]);
