b0d5a36c0cd00095615ba84a1175cf8b71973e69
jpirsch <jeanpirsch@gmail.com>
2020-05-19 14:53:19 +0200

    Composition without HOC, starting cv timeline in about page

--- src/Components/About/Timeline.js
+++ src/Components/About/Timeline.js
@@ -2,19 +2,18 @@
 import React from "react";
 
 class Timeline extends React.Component {
-
   render() {
     return (
       <div className="timeline">
         <div className="timeline__block">
           <div className="timeline__bullet"></div>
           <div className="timeline__header">
-            <p className="timeline__timeframe">July 2015 - Present</p>
-            <h3>Awesome Studio</h3>
-            <h5>Lead Designer</h5>
+            <p className="timeline__timeframe">{this.props.date}</p>
+            <h3>{this.props.company}</h3>
+            <h5>{this.props.role}</h5>
           </div>
           <div className="timeline__desc">
-            <p>Lorem ipsum Occaecat do esse ex et dolor culpa nisi ex in magna consectetur nisi cupidatat laboris esse eiusmod deserunt aute do quis velit esse sed Ut proident cupidatat nulla esse cillum laborum occaecat nostrud sit dolor incididunt amet est occaecat nisi.</p>
+            <p>{this.props.content}</p>
           </div>
         </div>
       </div>
@@ -23,3 +22,4 @@ class Timeline extends React.Component {
 }
 
 export default Timeline;
+
